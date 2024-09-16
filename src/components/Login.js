d916274ca4fe5d28a119c17814921b5d8a987c1c
Manekar Avantika Kishorrao (EXT) <avantikakishorrao.manekar@kone.com>
2024-09-16 14:49:21 +0530

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+function Login(){
+    return(
+        <div>
+            <h2>Login Page</h2>
+            <p>or <button >Sign up</button></p>
+          <p>Username</p>
+            <input type="text" placeholder="enter username"  ></input>
+          <p>Password</p>
+         <input type="password" placeholder="enter password" ></input><br></br>
+          <button  >Login</button>
+        </div>
+    )
+}
